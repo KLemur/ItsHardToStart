10a33e58beff9c087d648d0bb565c3016d125db1
Lemur <katania@student.agh.edu.pl>
2015-04-18 22:21:17 +0200

    Wczytywanie do tablicy odgórnie określonej ilości danych

--- src/klasaLemur.java
+++ src/klasaLemur.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class klasaLemur{
 	public static void main(String [] args){
-		System.out.println("Podaj imię");
-		String imie;
-		Scanner imieS = new Scanner(System.in);
-		imie=imieS.nextLine();
-		System.out.println("Witaj "+imie);
+		//System.out.println("Podaj ilość kolumn");
+		String [] dane;
+		dane = new String[3];
+
+		//int n;
+		//Scanner nS = new Scanner(System.in);
+		//n=nS.nextInt();
+		for (int i=0; i<dane.length; i++){
+			System.out.println("Podaj "+i+" daną\n");
+			Scanner tmpS=new Scanner(System.in);
+			dane [i]=tmpS.nextLine();
+		}
+		System.out.println("WYŚWIETLENIE\n");
+		for (int i=0; i<dane.length; i++){
+			System.out.println(i+" dana:\n"+dane[i]+"\n");
+		}
 	}
 }
